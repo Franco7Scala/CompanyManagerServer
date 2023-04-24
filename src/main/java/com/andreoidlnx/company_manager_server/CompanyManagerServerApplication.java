6f87a9757d13e5407177a0458deeab572eab85ac
Andrea Panebianco <andreoidlnx@gmail.com>
2023-04-24 15:01:10 +0200

    Spring Boot project started and connected to database

---
+++ src/main/java/com/andreoidlnx/company_manager_server/CompanyManagerServerApplication.java
@@ -0,0 +1,13 @@
+package com.andreoidlnx.company_manager_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyManagerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyManagerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andreoidlnx/company_manager_server/CompanyManagerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreoidlnx.company_manager_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyManagerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
